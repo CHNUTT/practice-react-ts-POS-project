de4616a63b19182e3265ecf5dfc3a2bd80b6faac
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2024-08-03 08:29:16 -0500

    use onAddItem in MainPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,192 @@
-import ItemCard from './components/ItemCard';
+import { ChangeEvent, useState } from 'react';
+import MainPanel from './components/MainPanel';
+import OrdersPanel from './components/OrdersPanel';
+import { Order } from './components/OrdersPanel/OrderItem/OrderItem';
+import { Item } from './components/MainPanel/CardItem/CardItem';
 
-const item = {
-  id: '1',
-  name: 'Beef Steak',
-  price: 20000,
-  image: 'https://playswellwithbutter.com/wp-content/uploads/2021/01/Cast-Iron-Steak-16.jpg',
+const items = [
+  {
+    id: '1',
+    name: 'Beef Steak',
+    price: 20,
+    image: 'https://playswellwithbutter.com/wp-content/uploads/2021/01/Cast-Iron-Steak-16.jpg',
+  },
+  {
+    id: '2',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+  {
+    id: '3',
+    name: 'Beef Steak',
+    price: 20,
+    image: 'https://playswellwithbutter.com/wp-content/uploads/2021/01/Cast-Iron-Steak-16.jpg',
+  },
+  {
+    id: '4',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+  {
+    id: '5',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+  {
+    id: '6',
+    name: 'Beef Steak',
+    price: 20,
+    image: 'https://playswellwithbutter.com/wp-content/uploads/2021/01/Cast-Iron-Steak-16.jpg',
+  },
+  {
+    id: '7',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+  {
+    id: '8',
+    name: 'Beef Steak',
+    price: 20,
+    image: 'https://playswellwithbutter.com/wp-content/uploads/2021/01/Cast-Iron-Steak-16.jpg',
+  },
+  {
+    id: '9',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+  {
+    id: '10',
+    name: 'Fried Chicken',
+    price: 18,
+    image: 'https://thegirlinspired.com/wp-content/uploads/2022/07/Fried-Chicken-28.jpg',
+  },
+];
+
+const itemsObj: { [key: string]: Item } = items.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {});
+
+type Cart = {
+  orders: Order[];
+  total: number;
 };
 
-const App = () => (
-  <div className="bg-gray-300 w-screen h-screen flex">
-    <ItemCard item={item} />
-  </div>
-);
+const intiOrders: Cart = {
+  orders: [
+    {
+      item: items[0],
+      qty: 1,
+    },
+    {
+      item: items[1],
+      qty: 2,
+    },
+    {
+      item: items[2],
+      qty: 3,
+    },
+  ],
+  total: 1 * items[0].price + 2 * items[1].price + 3 * items[2].price,
+};
+
+const App = () => {
+  const [orders, setOrders] = useState<Cart>(intiOrders);
+  const [customerName, setCustomerName] = useState<string>('');
+  const [tableNo, setTableNo] = useState<string>('');
+
+  const resetToInit = () => {
+    setOrders({ orders: [], total: 0 });
+    setCustomerName('');
+    setTableNo('');
+  };
+
+  const handleIncreaseQty = (id: string) => {
+    setOrders(({ orders, total }) => {
+      let price = 0;
+      const updateOrders = orders.map((orderItem) => {
+        if (orderItem.item.id === id) {
+          price = itemsObj[id].price;
+          return { ...orderItem, qty: orderItem.qty + 1 };
+        }
+        return orderItem;
+      });
+
+      return {
+        orders: updateOrders,
+        total: total + price,
+      };
+    });
+  };
+
+  const handleDecreaseQty = (id: string) => {
+    setOrders(({ orders, total }) => {
+      let price = 0;
+      const updateOrders = orders.map((orderItem) => {
+        if (orderItem.item.id === id && orderItem.qty > 1) {
+          price = itemsObj[id].price;
+          return { ...orderItem, qty: orderItem.qty + 1 };
+        }
+        return orderItem;
+      });
+
+      return {
+        orders: updateOrders,
+        total: total - price,
+      };
+    });
+  };
+
+  const handleRemoveItem = (id: string) => {
+    setOrders(({ orders, total }) => {
+      const removingItem = orders.find((order) => order.item.id === id)!;
+      const price = removingItem?.qty * removingItem?.item.price;
+
+      const updatedOrders = orders.filter((order) => order.item.id !== id);
+
+      return {
+        orders: updatedOrders,
+        total: total - price,
+      };
+    });
+  };
+
+  const handlePressOrder = () => {
+    console.log('orders: ', orders);
+    console.log('customerName: ', customerName);
+    console.log('tableNo: ', tableNo);
+    resetToInit();
+  };
+
+  const handleCancelOrder = () => {
+    resetToInit();
+  };
+
+  const handleCustomerName = (e: ChangeEvent<HTMLInputElement>) => {
+    setCustomerName(e.target.value);
+  };
+
+  const handleTableNo = (e: ChangeEvent<HTMLInputElement>) => {
+    setTableNo(e.target.value);
+  };
+
+  return (
+    <div className="bg-[#bbbbbb2a] min-w-screen max-w-screen min-h-screen flex justify-between">
+      <MainPanel items={items} />
+      <OrdersPanel
+        cart={orders}
+        onIncrease={handleIncreaseQty}
+        onDecrease={handleDecreaseQty}
+        onRemove={handleRemoveItem}
+        onOrder={handlePressOrder}
+        onCancel={handleCancelOrder}
+        onNameChange={handleCustomerName}
+        onTableChange={handleTableNo}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/MainPanel/MainPanel.tsx
+++ src/components/MainPanel/MainPanel.tsx
@@ -2,15 +2,16 @@ import CardItem, { Item } from './CardItem/CardItem';
 
 type MainPanelProp = {
   items: Item[];
+  onAddItem: () => void;
 };
 
-const MainPanel = ({ items }: MainPanelProp) => {
+const MainPanel = ({ items, onAddItem: handleOnAddItem }: MainPanelProp) => {
   return (
     <div className="container mx-auto">
       <div className="grid gap-4 p-6 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {items.map((item) => (
           <div key={item.id}>
-            <CardItem item={item} />
+            <CardItem item={item} onAddItem={handleOnAddItem} />
           </div>
         ))}
       </div>

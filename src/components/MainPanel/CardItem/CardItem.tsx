7c78adbb0db889d7d1a85e714455c693eb86ea4e
Cheer Chokwittaya <65862558+CHNUTT@users.noreply.github.com>
2024-08-06 11:07:12 -0500

    ISSUE-12: Put things together with mocked data on App Component (#14)

--- src/components/MainPanel/CardItem/CardItem.tsx
+++ src/components/MainPanel/CardItem/CardItem.tsx
@@ -11,28 +11,30 @@ export type Item = {
 
 type CardItemProps = {
   item: Item;
-  onAddItem: () => void;
+  onAddItem: (id: string) => void;
 };
 
-const CardItem = ({ item: { name, price, image }, onAddItem }: CardItemProps) => {
+const CardItem = ({ item: { id, name, price, image }, onAddItem: handleOnAddItem }: CardItemProps) => {
   return (
     <div
       role="presentation"
       aria-labelledby="item-name"
-      className="h-full rounded-2xl bg-white flex flex-col items-center justify-between py-6 px-4"
+      className="h-full rounded-2xl bg-white flex flex-col items-start justify-between py-6 px-4"
     >
       <img
         src={image}
         alt={name}
         className="rounded-full w-[120px] md:w-[168px] h-[120px] md:h-[168px] object-center object-cover"
       />
-      <label id="item-name" className="mt-[30px] font-bold text-xl xl:text-2xl text-center">
-        {name}
-      </label>
-      <span className="mt-[10px] font-extrabold text-xl xl:text-2xl text-[#FFCA40]">
-        {formatter.format(price)}
-      </span>
-      <Button onClick={onAddItem} label="Add to Orders" />
+      <div className="flex flex-col items-center justify-between w-full">
+        <label id="item-name" className="mt-[30px] font-bold text-md xl:text-xl text-start w-full">
+          {name}
+        </label>
+        <span className="mt-14 font-extrabold text-xl xl:text-2xl text-[#FFCA40]">
+          {formatter.format(price)}
+        </span>
+      </div>
+      <Button onClick={() => handleOnAddItem(id)} label="Add to Orders" />
     </div>
   );
 };

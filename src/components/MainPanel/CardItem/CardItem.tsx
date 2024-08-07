0f3fb31754daafc1daedbf34c1c24fd483eadc77
Cheer Chokwittaya <65862558+CHNUTT@users.noreply.github.com>
2024-08-07 08:26:22 -0500

    ISSUE-15: update styles of CardItem (#16)

--- src/components/MainPanel/CardItem/CardItem.tsx
+++ src/components/MainPanel/CardItem/CardItem.tsx
@@ -19,18 +19,18 @@ const CardItem = ({ item: { id, name, price, image }, onAddItem: handleOnAddItem
     <div
       role="presentation"
       aria-labelledby="item-name"
-      className="h-full rounded-2xl bg-white flex flex-col items-start justify-between py-6 px-4"
+      className="h-full rounded-2xl bg-white flex flex-col items-center justify-start py-6 px-4"
     >
       <img
         src={image}
         alt={name}
         className="rounded-full w-[120px] md:w-[168px] h-[120px] md:h-[168px] object-center object-cover"
       />
-      <div className="flex flex-col items-center justify-between w-full">
-        <label id="item-name" className="mt-[30px] font-bold text-md xl:text-xl text-start w-full">
+      <div className="flex flex-col items-center justify-between w-full flex-1 mb-10">
+        <label id="item-name" className="mt-[30px] font-bold text-md xl:text-xl text-center w-full">
           {name}
         </label>
-        <span className="mt-14 font-extrabold text-xl xl:text-2xl text-[#FFCA40]">
+        <span className="mt-4 font-extrabold text-xl xl:text-2xl text-[#FFCA40]">
           {formatter.format(price)}
         </span>
       </div>
